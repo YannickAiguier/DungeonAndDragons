27c50143ad9394817eb3abdbf9793c75d0b28f3c
YannickAiguier <yannick.aiguier@gmail.com>
2021-04-29 14:41:26 +0200

    Classe Player : les 3 constructeurs #4

--- src/Player.java
+++ src/Player.java
@@ -6,7 +6,19 @@ public class Player {
 	int attack;
 	int baseAttack;
 	MeanOfAttack attackType;
-	String shield;
+	String protectionType;
 	
+	public Player() {
+		name = "";
+	}
 	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	public Player(String name, int life, int attack) {
+		this.name = name;
+		this.life = life;
+		this.attack = attack;
+	}
 }
