b24d7b2cd573759b810138b20b2eb23eabdc7d1b
YannickAiguier <yannick.aiguier@gmail.com>
2021-04-30 14:37:17 +0200

    Gestion d'exception implémentée #21

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -25,10 +25,15 @@ public class GameBoard {
 	
 	// méthode qui fait avancer le joueur d'un D6 
 	public void advancePlayer() {
-		playerPos += MyUtils.rollDice(6);
-		if (playerPos > 63) {
+		try {
+			playerPos += MyUtils.rollDice(6);
+			if (playerPos > 63) {
+				throw new PersonnageHorsPlateauException("Impossible de dépasser la case 64");
+			}
+		} catch (PersonnageHorsPlateauException e){
 			playerPos = 63;
 		}
+		board[playerPos] = 1;
 	}
 		// méthode qui vérifie si un joueur est arrivé au bout du plateau
 	public boolean playerNotOnLastBox() {
