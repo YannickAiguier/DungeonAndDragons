f2ff218a4869c4dc6f4d25d3e720843e2f5b8bfd
YannickAiguier <yannick.aiguier@gmail.com>
2021-04-30 09:10:05 +0200

    Classe Weapon et Spell : héritent de MeanOfAttack #2

---
+++ src/Spell.java
@@ -0,0 +1,9 @@
+
+public class Spell extends MeanOfAttack {
+
+	// constructeur, utilise celui de la classe parente MeanOfAttack
+	public Spell(String name, int attack) {
+		super(name, attack);
+	}
+	
+}
