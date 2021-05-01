2b3c403b25d323bda599657702bdfdfb2fd83aa5
YannickAiguier <yannick.aiguier@gmail.com>
2021-05-01 11:03:39 +0200

    Classe MeanOfAttack : devient abstraite, modification de toString #27

---
+++ src/MeanOfAttack.java
@@ -0,0 +1,47 @@
+
+public abstract class MeanOfAttack {
+
+	String name;
+	int attack;
+	
+	/**
+	 * Constructeur
+	 * @param name
+	 * @param attack
+	 */
+	public MeanOfAttack(String name, int attack) {
+		this.name = name;
+		this.attack = attack;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the attack
+	 */
+	public int getAttack() {
+		return attack;
+	}
+	/**
+	 * @param attack the attack to set
+	 */
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	@Override
+	public String toString() {
+		return this.getClass().getName() + " [name=" + name + ", attack=" + attack + "]";
+	}
+	
+	
+
+}
