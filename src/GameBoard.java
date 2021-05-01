0bf472b96190ffcd154dc6a12577e23809247a09
YannickAiguier <yannick.aiguier@gmail.com>
2021-05-01 14:34:13 +0200

    Classe GameBoard : modification de la structure (tableau d'objets) et méthodes pour lire et écrire dans une case #32

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
 
 public class GameBoard {
-	
-	int[] board;
+
+	Object[] board;
 	int playerPos;
-	
+
 	// Constructeur : initialisation du plateau à 64 cases, joueur en case 1
 	public GameBoard() {
-		board = new int[64];
+		board = new Object[64];
+		System.out.println(Arrays.toString(board));
 		playerPos = 0;
 	}
 
@@ -22,22 +24,32 @@ public class GameBoard {
 	public String toString() {
 		return " en case " + (playerPos + 1) + " sur 64.";
 	}
-	
-	// méthode qui fait avancer le joueur d'un D6 
+
+	// méthode qui fait avancer le joueur d'un D6
 	public void advancePlayer() {
 		try {
 			playerPos += MyUtils.rollDice(6);
 			if (playerPos > 63) {
 				throw new PersonnageHorsPlateauException("Impossible de dépasser la case 64");
 			}
-		} catch (PersonnageHorsPlateauException e){
+		} catch (PersonnageHorsPlateauException e) {
 			playerPos = 63;
 		}
-		board[playerPos] = 1;
 	}
-		// méthode qui vérifie si un joueur est arrivé au bout du plateau
+
+	// méthode qui vérifie si un joueur est arrivé au bout du plateau
 	public boolean playerNotOnLastBox() {
 		return playerPos != 63;
 	}
-		
+	
+	// méthode qui retourne le contenu de la case où se trouve le joueur
+	public Object getBox() {
+		return board[playerPos];
+	}
+	
+	// méthode qui met un objet dans la case board[index]
+	public void setBox(int index, Object object) {
+		board[index] = object;
+	}
+
 }
