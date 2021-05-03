bd3e07905fc83c1c0fc5acfb801cf63afdcad035
YannickAiguier <yannick.aiguier@gmail.com>
2021-05-03 09:21:11 +0200

    Classes Gobelin, Sorcerer, Dragon implémentées #45

---
+++ src/Dragon.java
@@ -0,0 +1,8 @@
+
+public class Dragon extends Monster {
+	
+	public Dragon(int life, int attack) {
+		super(life, attack);
+	}
+
+}
---
+++ src/Gobelin.java
@@ -0,0 +1,8 @@
+
+public class Gobelin extends Monster {
+	
+	public Gobelin(int life, int attack) {
+		super(life, attack);
+	}
+
+}
