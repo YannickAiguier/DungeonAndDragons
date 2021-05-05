37d761ad861f7e5c9a3997bffbcfa40cfe332ec4
YannickAiguier <yannick.aiguier@gmail.com>
2021-05-05 16:31:44 +0200

    tmp : push pour campus

---
+++ src/consoleViewer.java
@@ -0,0 +1,34 @@
+
+public class consoleViewer extends Viewer {
+
+	@Override
+	public void showMove(int dice, int playerPosition) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showDetail() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showPlayer(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showBox(Box box) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
